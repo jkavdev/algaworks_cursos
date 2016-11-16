e9a239314c8708b94b187e616e9be7efb85c39d1
jkavdev <jhonatankolen@gmail.com>
2016-11-16 11:01:44 -0200

    v4.9. Mais do mapeamento Muitos-Para-Um - Alterando o atributos do relacionamento entre ModeloCarro e Fabricante com @JoinColumn

--- algaworks-locadora-veiculo-web/src/main/java/br/com/jkavdev/algaworks/jpa2/modelos/ModeloCarro.java
+++ algaworks-locadora-veiculo-web/src/main/java/br/com/jkavdev/algaworks/jpa2/modelos/ModeloCarro.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
@@ -48,6 +49,7 @@ public class ModeloCarro implements Serializable {
 	}
 
 	@ManyToOne
+	@JoinColumn(name = "codigo_fabricante")
 	public Fabricante getFabricante() {
 		return fabricante;
 	}
