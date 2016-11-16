a9ad8a6bb66dcb0f409310cf43aef72e5a708645
jkavdev <jhonatankolen@gmail.com>
2016-11-16 14:05:25 -0200

    v4.15-implementando-mais-uma-consulta-v1 - Realizando pesquisa de Carro

---
+++ algaworks-locadora-veiculo-web/src/main/java/br/com/jkavdev/algaworks/jpa2/beans/PesquisaCarroBean.java
@@ -0,0 +1,55 @@
+package br.com.jkavdev.algaworks.jpa2.beans;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.jkavdev.algaworks.jpa2.daos.CarroDao;
+import br.com.jkavdev.algaworks.jpa2.modelos.Carro;
+import br.com.jkavdev.algaworks.jpa2.services.NegocioException;
+import br.com.jkavdev.algaworks.jpa2.util.jsf.FacesUtil;
+
+@Named
+@ViewScoped
+public class PesquisaCarroBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private CarroDao carroDao;
+
+	private List<Carro> carros;
+	private Carro carroSelecionado;
+
+	@PostConstruct
+	public void init() {
+		carros = carroDao.buscarTodos();
+	}
+
+	public void excluir() {
+		try {
+			carroDao.excluir(carroSelecionado);
+			carros.remove(carroSelecionado);
+			FacesUtil.addSuccessMessage("Carro removido com sucesso!");
+		} catch (NegocioException e) {
+			FacesUtil.addErrorMessage(e.getMessage());
+		}
+	}
+
+	public Carro getCarroSelecionado() {
+		return carroSelecionado;
+	}
+
+	public void setCarroSelecionado(Carro carroSelecionado) {
+		this.carroSelecionado = carroSelecionado;
+	}
+
+	public List<Carro> getCarros() {
+		return carros;
+	}
+
+}
