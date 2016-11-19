c928955884f18e147611a8014adb5456fe3a37c9
jkavdev <jhonatankolen@gmail.com>
2016-11-19 10:20:52 -0200

    v11.5-consultas-nativas-v1 - Usando querys nativas

---
+++ algaworks-jpa2-locadora-veiculo-web/src/test/java/br/com/jkavdev/algaworks/jpa2/test/consultas/ConsultasNativas.java
@@ -0,0 +1,24 @@
+package br.com.jkavdev.algaworks.jpa2.test.consultas;
+
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.junit.Test;
+
+import br.com.jkavdev.algaworks.jpa2.modelos.Carro;
+import br.com.jkavdev.algaworks.jpa2.test.JunitJpaConfig;
+
+public class ConsultasNativas extends JunitJpaConfig {
+
+	@Test
+	public void consultaNativa() {
+		Query nativeQuery = getManager().createNativeQuery("select * from Carros", Carro.class);
+		List<Carro> carros = nativeQuery.getResultList();
+
+		for (Carro carro : carros) {
+			System.out.println("Placa: " + carro.getPlaca());
+		}
+	}
+
+}
