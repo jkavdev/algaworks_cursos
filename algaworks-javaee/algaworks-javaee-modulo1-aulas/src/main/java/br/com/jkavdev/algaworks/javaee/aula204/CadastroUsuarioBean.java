02cf0b3fb0ead83723a9f651378a223eb590647c
jkavdev <jhonatankolen@gmail.com>
2017-01-14 10:57:45 -0200

    2.4 - Submissao parcial - Realizando o submit parcial apenas dos componentes indicados, e configuração do web.xml para submit parcial do primefaces

---
+++ algaworks-javaee/algaworks-javaee-modulo1-aulas/src/main/java/br/com/jkavdev/algaworks/javaee/aula204/CadastroUsuarioBean.java
@@ -0,0 +1,56 @@
+package br.com.jkavdev.algaworks.javaee.aula204;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "cadastroUsuarioPartialBean")
+@ViewScoped
+public class CadastroUsuarioBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String login;
+	private String nome;
+
+	public void cadastrar() {
+		System.out.println("Login: " + login);
+		System.out.println("Nome: " + nome);
+
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cadastro realizado com Sucesso"));
+	}
+
+	public void verificarDisponibilidadeLogin() {
+		FacesMessage msg = null;
+		System.out.println("Verificando disponibilidade: " + this.login);
+
+		if ("joao".equalsIgnoreCase(this.login)) {
+			msg = new FacesMessage("Login já está em uso");
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+		} else {
+			msg = new FacesMessage("Login disponível");
+		}
+
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
