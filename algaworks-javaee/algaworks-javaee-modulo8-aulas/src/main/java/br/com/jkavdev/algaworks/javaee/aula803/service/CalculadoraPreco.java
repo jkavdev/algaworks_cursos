274acd0600d7c49d078aba073b083bcd15bfddf5
jkavdev <jhonatankolen@gmail.com>
2017-01-19 22:40:25 -0200

    8.3 - Configurando um projeto com jsf e cdi

---
+++ algaworks-javaee/algaworks-javaee-modulo8-aulas/src/main/java/br/com/jkavdev/algaworks/javaee/aula803/service/CalculadoraPreco.java
@@ -0,0 +1,9 @@
+package br.com.jkavdev.algaworks.javaee.aula803.service;
+
+public class CalculadoraPreco {
+	
+	public double calcularPreco(int quantidade, double precoUnitario){
+		return quantidade * precoUnitario;
+	}
+
+}
