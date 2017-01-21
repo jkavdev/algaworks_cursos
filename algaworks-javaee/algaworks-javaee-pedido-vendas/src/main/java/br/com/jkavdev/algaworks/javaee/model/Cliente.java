841fdbc5872b197d725f76d40136df1e93786c62
jkavdev <jhonatankolen@gmail.com>
2017-01-21 15:04:04 -0200

    10.3 - Criando entidades do cadastro de clientes

---
+++ algaworks-javaee/algaworks-javaee-pedido-vendas/src/main/java/br/com/jkavdev/algaworks/javaee/model/Cliente.java
@@ -0,0 +1,65 @@
+package br.com.jkavdev.algaworks.javaee.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String nome;
+	private String email;
+	private String documentoReceitaFederal;
+	private TipoPessoa tipo;
+	private List<Endereco> enderecos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDocumentoReceitaFederal() {
+		return documentoReceitaFederal;
+	}
+
+	public void setDocumentoReceitaFederal(String documentoReceitaFederal) {
+		this.documentoReceitaFederal = documentoReceitaFederal;
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	public TipoPessoa getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoPessoa tipo) {
+		this.tipo = tipo;
+	}
+
+}
---
+++ algaworks-javaee/algaworks-javaee-pedido-vendas/src/main/java/br/com/jkavdev/algaworks/javaee/model/TipoPessoa.java
@@ -0,0 +1,7 @@
+package br.com.jkavdev.algaworks.javaee.model;
+
+public enum TipoPessoa {
+
+	FISICA, JURIDICA
+
+}
