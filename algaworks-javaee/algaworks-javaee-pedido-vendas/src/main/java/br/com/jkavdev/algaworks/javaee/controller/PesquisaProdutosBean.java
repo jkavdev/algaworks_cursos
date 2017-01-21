e6f0c0b7c78f1c0c1b23d66d111d9a67d8855620
jkavdev <jhonatankolen@gmail.com>
2017-01-21 10:01:47 -0200

    8.8 - Usando o viewscoped com cdi - Adicionando CDI ao projeto pedido vendas e incluindo @ViewScoped ao CDI

--- algaworks-javaee/algaworks-javaee-pedido-vendas/src/main/java/br/com/jkavdev/algaworks/javaee/controller/PesquisaProdutosBean.java
+++ algaworks-javaee/algaworks-javaee-pedido-vendas/src/main/java/br/com/jkavdev/algaworks/javaee/controller/PesquisaProdutosBean.java
@@ -4,10 +4,10 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
 
-@ManagedBean
+@Named
 @ViewScoped
 public class PesquisaProdutosBean implements Serializable {
 
