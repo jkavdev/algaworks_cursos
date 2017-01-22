18d14b5e6c69947061178c43655eca211934e2e7
jkavdev <jhonatankolen@gmail.com>
2017-01-22 12:30:35 -0200

    11.4 - Adicionando restricoes no pedido

--- algaworks-javaee/algaworks-javaee-pedido-vendas/src/main/java/br/com/jkavdev/algaworks/javaee/controller/CadastroPedidoBean.java
+++ algaworks-javaee/algaworks-javaee-pedido-vendas/src/main/java/br/com/jkavdev/algaworks/javaee/controller/CadastroPedidoBean.java
@@ -7,7 +7,7 @@ import java.util.List;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
 
-import br.com.jkavdev.algaworks.javaee.service.NegocioException;
+import br.com.jkavdev.algaworks.javaee.model.Pedido;
 
 @Named
 @ViewScoped
@@ -15,15 +15,20 @@ public class CadastroPedidoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	private Pedido pedido;
 	private List<Integer> itens;
 
 	public CadastroPedidoBean() {
+		pedido = new Pedido();
 		itens = new ArrayList<Integer>();
 		itens.add(1);
 	}
 
 	public void salvar() {
-		throw new NegocioException("Exceção Negocio: cadastro de pedidos não implementado");
+	}
+
+	public Pedido getPedido() {
+		return pedido;
 	}
 
 	public List<Integer> getItens() {
