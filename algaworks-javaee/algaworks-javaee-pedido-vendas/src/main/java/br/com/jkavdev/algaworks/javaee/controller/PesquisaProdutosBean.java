14d44198da385fdfbcb8c9446e0c449bac80b1b2
jkavdev <jhonatankolen@gmail.com>
2017-01-22 19:17:59 -0200

    12.8 - Implementando a pesquisa de produtos

--- algaworks-javaee/algaworks-javaee-pedido-vendas/src/main/java/br/com/jkavdev/algaworks/javaee/controller/PesquisaProdutosBean.java
+++ algaworks-javaee/algaworks-javaee-pedido-vendas/src/main/java/br/com/jkavdev/algaworks/javaee/controller/PesquisaProdutosBean.java
@@ -1,11 +1,15 @@
 package br.com.jkavdev.algaworks.javaee.controller;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import br.com.jkavdev.algaworks.javaee.model.Produto;
+import br.com.jkavdev.algaworks.javaee.repository.Produtos;
+import br.com.jkavdev.algaworks.javaee.repository.filter.ProdutoFilter;
 
 @Named
 @ViewScoped
@@ -13,15 +17,29 @@ public class PesquisaProdutosBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private List<Integer> produtosFiltrados = new ArrayList<Integer>();
+	@Inject
+	private Produtos produtos;
+
+	private ProdutoFilter filtro;
+	private List<Produto> produtosFiltrados;
 
 	public PesquisaProdutosBean() {
-		for (int i = 0; i < 50; i++) {
-			produtosFiltrados.add(i);
-		}
+		filtro = new ProdutoFilter();
 	}
 
-	public List<Integer> getProdutosFiltrados() {
+	public void pesquisar() {
+		produtosFiltrados = produtos.filtrados(filtro);
+	}
+
+	public ProdutoFilter getFiltro() {
+		return filtro;
+	}
+
+	public void setFiltro(ProdutoFilter filtro) {
+		this.filtro = filtro;
+	}
+
+	public List<Produto> getProdutosFiltrados() {
 		return produtosFiltrados;
 	}
 
