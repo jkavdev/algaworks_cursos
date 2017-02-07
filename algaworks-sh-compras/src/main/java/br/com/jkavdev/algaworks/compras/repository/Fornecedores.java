ae1cf50689671f8669fb3764e96cba2d80970b3e
Jhonatan Kolen <jhonatankolen@gmail.com>
2017-02-07 11:20:35 -0200

    Vídeo 3 - 42:00 - Problema do N+1 e caching - Criando interceptador para abrir e commitar a transação com o banco, retirando o controle de transação do método adicionar de fornecedores, e passando para o interceptor

--- algaworks-sh-compras/src/main/java/br/com/jkavdev/algaworks/compras/repository/Fornecedores.java
+++ algaworks-sh-compras/src/main/java/br/com/jkavdev/algaworks/compras/repository/Fornecedores.java
@@ -5,28 +5,20 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 
 import br.com.jkavdev.algaworks.compras.model.Fornecedor;
+import br.com.jkavdev.algaworks.compras.util.jpa.Transactional;
 
 public class Fornecedores implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Inject
-	private EntityManagerFactory factory;
-	
-	@Inject
 	private EntityManager manager;
 
+	@Transactional
 	public void adicionar(Fornecedor fornecedor) {
-		EntityManager manager = this.factory.createEntityManager();
-
-		manager.getTransaction().begin();
 		manager.persist(fornecedor);
-		manager.getTransaction().commit();
-
-		manager.close();
 	}
 
 	public List<Fornecedor> todosTodosComCidadeEEstado() {
