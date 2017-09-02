f9df22a2670465b34646819cae3edbe4748a3ef2
jkavdev <jhonatankolen@gmail.com>
2017-09-01 22:13:09 -0300

    3.5-colecao-vazia-o-que-retornar-v1
    - Retornando estatus da requisição junto com o conteúdo requisitado

--- algaworks-spring-angular/algaworks-algamoney-api/src/main/java/br/com/jkavdev/algaworks/resource/CategoriaResource.java
+++ algaworks-spring-angular/algaworks-algamoney-api/src/main/java/br/com/jkavdev/algaworks/resource/CategoriaResource.java
@@ -3,6 +3,7 @@ package br.com.jkavdev.algaworks.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,25 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
+	}
+
+	@GetMapping("/comConteudo")
+	public ResponseEntity<?> listarComCodigo() {
+		List<Categoria> categorias = categoriaRepository.findAll();
+		
+		//podemos retornar um status junto com o conteudo requisitado
+		//aqui estamos verificando se retornou alguma coisa
+		//se sim devolva o resultado com o status de ok
+		
+		//se nao tiver nada, devolva um resultado indicando que nao foi encontrado nada
+		//mas fica estranho retornar o 404
+//		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.notFound().build();
+		
+		//Podemos devolver um status que indica que nao houve uma resposta para o conteudo requisitado
+		//mas mesmo assim, continuamos a ter um resultado como vazio e apenas o status retornado
+		//muitas das vezes podemos apenas retornar o conteudo vazio mesmo
+		//categorias = {}
+		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
 	}
 
 }
