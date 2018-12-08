c3c3b59c835434774aa65bb815493c5f20adbc2f
jkavdev <jhonatankolen@gmail.com>
2018-12-08 00:34:13 -0200

    Começando com o AngularJS - removendo filme com ng-click

--- algaworks-angularjs/comecando-com-angularjs/js/filmes.controller.js
+++ algaworks-angularjs/comecando-com-angularjs/js/filmes.controller.js
@@ -10,7 +10,23 @@ angular.module('Filmes').controller('FilmesController', function ($scope) {
             produtora: "Fox",
             sinopse: "Baseado no anti-herói não convencional da Marvel Comics, Deadpool conta a história da origem do ex-agente das Forças Especiais que se tornou o mercenário Wade Wilson. Depois de ser submetido a um desonesto experimento que o deixa com poderes de cura acelerada, Wade adota o alter ego de Deadpool. Armado com suas novas habilidades e um senso de humor negro e distorcido, Deadpool persegue o homem que quase destruiu sua vida.",
             cartaz: "http://www.mediaplus.com.br/clicfolha2016/uploads/imagem_arquivo/918856_cine1.png"
+        },
+        {
+            id: "3211123",
+            titulo: "Deadpool",
+            ano: "2016",
+            produtora: "Fox",
+            sinopse: "Baseado no anti-herói não convencional da Marvel Comics, Deadpool conta a história da origem do ex-agente das Forças Especiais que se tornou o mercenário Wade Wilson. Depois de ser submetido a um desonesto experimento que o deixa com poderes de cura acelerada, Wade adota o alter ego de Deadpool. Armado com suas novas habilidades e um senso de humor negro e distorcido, Deadpool persegue o homem que quase destruiu sua vida.",
+            cartaz: "http://www.mediaplus.com.br/clicfolha2016/uploads/imagem_arquivo/918856_cine1.png"
         }
     ];
+
+    $scope.removerFilme = function (id) {
+        angular.forEach($scope.filmes, (filme, i) => {
+            if (filme.id == id) {
+                $scope.filmes.splice(i, 1);
+            }
+        });
+    }
 
 });
