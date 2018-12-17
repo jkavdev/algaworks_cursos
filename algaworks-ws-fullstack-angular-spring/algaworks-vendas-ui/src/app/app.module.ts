d7272b58653aa132eefe4f95a5e6b9f7824a028f
Jhonatan Kolen <jhonatan.viana@spread.com.br>
2018-12-17 08:32:16 -0200

    Algaworks - Workshop Fullstack Angular Spring - Vendas
    - adicionando locale pt-br
    - formatando data e numero total para formato pt-br

--- algaworks-ws-fullstack-angular-spring/algaworks-vendas-ui/src/app/app.module.ts
+++ algaworks-ws-fullstack-angular-spring/algaworks-vendas-ui/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgModule} from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import {HttpClientModule} from "@angular/common/http";
+import localePt from "@angular/common/locales/pt";
+import {registerLocaleData} from "@angular/common";
 
 import {TableModule} from "primeng/components/table/table";
 
 import {AppComponent} from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {VendasListagemComponent} from './vendas-listagem/vendas-listagem.component';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -21,7 +25,9 @@ import {VendasListagemComponent} from './vendas-listagem/vendas-listagem.compone
     HttpClientModule,
     TableModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
