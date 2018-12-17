954ca7be986f2f38bfff0ff09aea5507daf56186
jkavdev <jhonatankolen@gmail.com>
2018-12-17 02:34:09 -0200

    Algaworks - Workshop Fullstack Angular Spring - Vendas
    - definindo origens de requisicoes aceitas na api

--- algaworks-ws-fullstack-angular-spring/algaworks-vendas-api/src/main/java/br/com/jkavdev/algaworks/fullstack/vendas/resource/ProdutosResource.java
+++ algaworks-ws-fullstack-angular-spring/algaworks-vendas-api/src/main/java/br/com/jkavdev/algaworks/fullstack/vendas/resource/ProdutosResource.java
@@ -3,12 +3,14 @@ package br.com.jkavdev.algaworks.fullstack.vendas.resource;
 import br.com.jkavdev.algaworks.fullstack.vendas.model.Produto;
 import br.com.jkavdev.algaworks.fullstack.vendas.repository.Produtos;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/produtos")
 public class ProdutosResource {
