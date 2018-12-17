703a8fe0292b0eb2e055ced1fbd68fc0819a86b0
jkavdev <jhonatankolen@gmail.com>
2018-12-17 03:17:47 -0200

    Algaworks - Workshop Fullstack Angular Spring - Vendas
    - adicionando componente de listagem de vendas
    - adicionando o primeng

--- algaworks-ws-fullstack-angular-spring/algaworks-vendas-ui/src/app/app.module.ts
+++ algaworks-ws-fullstack-angular-spring/algaworks-vendas-ui/src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import {TableModule} from "primeng/components/table/table";
+
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {VendasListagemComponent} from './vendas-listagem/vendas-listagem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    VendasListagemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
