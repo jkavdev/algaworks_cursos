3fc461a6fefc6b805c1f6eecd023a8b411475fa8
JHONATAN KOLEN ALVES VIANA <f930649@mail.caixa>
2020-04-25 20:50:31 -0300

    Persistência, Bean Validation e Exception Handler - Part 1 - 2020-04
    ajustes

--- algaworks-sri-osworks/algaworks-osworks-api/src/main/java/br/com/jkavdev/algaworks/osworks/domain/repository/ClienteRepository.java
+++ algaworks-sri-osworks/algaworks-osworks-api/src/main/java/br/com/jkavdev/algaworks/osworks/domain/repository/ClienteRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.jkavdev.algaworks.osworks.model.Cliente;
+import br.com.jkavdev.algaworks.osworks.domain.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
