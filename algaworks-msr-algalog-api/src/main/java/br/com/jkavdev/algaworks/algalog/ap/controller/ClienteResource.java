e5e8d90f59e6860587c4232987aa429452070c54
jkavdev <jhonatankolen@gmail.com>
2021-08-18 11:06:22 -0300

    [LIESR] CPL2.2 - Usando Jakarta Persistence JPA

--- algaworks-msr-algalog-api/src/main/java/br/com/jkavdev/algaworks/algalog/ap/controller/ClienteResource.java
+++ algaworks-msr-algalog-api/src/main/java/br/com/jkavdev/algaworks/algalog/ap/controller/ClienteResource.java
@@ -1,7 +1,9 @@
 package br.com.jkavdev.algaworks.algalog.ap.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,19 +13,12 @@ import br.com.jkavdev.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteResource {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	private List<Cliente> listar(){
-		
-		Cliente cliente = new Cliente();
-		cliente.setNome("Jhonatan");
-		cliente.setTelefone("99999999");
-		cliente.setId(1L);
-		Cliente cliente2 = new Cliente();
-		cliente2.setNome("Jhonatan");
-		cliente2.setTelefone("99999999");
-		cliente2.setId(1L);
-		
-		return Arrays.asList(cliente, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 
 }
