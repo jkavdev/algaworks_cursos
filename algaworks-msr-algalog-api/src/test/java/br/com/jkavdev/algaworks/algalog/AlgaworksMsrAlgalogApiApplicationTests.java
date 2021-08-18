46b4a116b6e6cee950bb9646791ab8bc48e41db5
jkavdev <jhonatankolen@gmail.com>
2021-08-18 09:38:53 -0300

    [LIESR] CPL1.3 - Criando o projeto Spring Boot

---
+++ algaworks-msr-algalog-api/src/test/java/br/com/jkavdev/algaworks/algalog/AlgaworksMsrAlgalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jkavdev.algaworks.algalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaworksMsrAlgalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
