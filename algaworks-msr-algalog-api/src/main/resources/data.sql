38bfa9e777bcec3242b54a6f51594af45edc4ab8
jkavdev <jhonatankolen@gmail.com>
2021-08-24 10:12:01 -0300

    [LIESR.8] CPL2.1 - Configurando o Flyway - configurando o jpa com o banco h2, e adicionando o flyway e script de insert de clientes

---
+++ algaworks-msr-algalog-api/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into cliente(nome, email, telefone) values('Jhonatan', 'jhonatan@email.com', '999999999');
+insert into cliente(nome, email, telefone) values('lucas', 'lucas@email.com', '999999999');
+insert into cliente(nome, email, telefone) values('douglas', 'douglas@email.com', '999999999');
