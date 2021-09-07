fc5b72d11f0d395d8146a204eecc6c90c4c60e52
jkavdev <jhonatankolen@gmail.com>
2021-09-07 15:57:36 -0300

    [LIESR] CPL3.6 - Implementando sub recursos - listando ocorrencias da entrega

--- algaworks-msr-algalog-api/src/main/java/br/com/jkavdev/algaworks/algalog/api/controller/OcorrenciaController.java
+++ algaworks-msr-algalog-api/src/main/java/br/com/jkavdev/algaworks/algalog/api/controller/OcorrenciaController.java
@@ -1,9 +1,12 @@
 package br.com.jkavdev.algaworks.algalog.api.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.jkavdev.algaworks.algalog.api.assembler.OcorrenciaAssembler;
 import br.com.jkavdev.algaworks.algalog.api.input.OcorrenciaInput;
 import br.com.jkavdev.algaworks.algalog.api.model.OcorrenciaModel;
+import br.com.jkavdev.algaworks.algalog.domain.model.Entrega;
+import br.com.jkavdev.algaworks.algalog.domain.service.BuscaEntregaService;
 import br.com.jkavdev.algaworks.algalog.domain.service.RegistroOcorrenciaService;
 
 @RestController
@@ -26,10 +31,19 @@ public class OcorrenciaController {
 	@Autowired
 	private OcorrenciaAssembler mapper;
 
+	@Autowired
+	private BuscaEntregaService buscaEntregaService;
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public OcorrenciaModel registrar(@PathVariable Long entregaId, @Valid @RequestBody OcorrenciaInput input) {
 		return mapper.toModel(ocorrenciaService.registrar(entregaId, input.getDescricao()));
+	}
+
+	@GetMapping
+	public List<OcorrenciaModel> listar(@PathVariable Long entregaId) {
+		Entrega entrega = buscaEntregaService.buscar(entregaId);
+		return mapper.toCollectionModel(entrega.getOcorrencias());
 	}
 
 }
