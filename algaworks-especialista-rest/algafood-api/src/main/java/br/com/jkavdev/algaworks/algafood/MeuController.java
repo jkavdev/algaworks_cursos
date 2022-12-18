9d4bb7613ec7d2a7048c7fbef5cf802c88d258fd
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2022-12-18 19:55:17 -0300

    Algaworks - Especialista Spring REST - 2.12. Injetando dependências (beans Spring)

--- algaworks-especialista-rest/algafood-api/src/main/java/br/com/jkavdev/algaworks/algafood/MeuController.java
+++ algaworks-especialista-rest/algafood-api/src/main/java/br/com/jkavdev/algaworks/algafood/MeuController.java
@@ -4,16 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import br.com.jkavdev.algaworks.algafood.modelo.Cliente;
+import br.com.jkavdev.algaworks.algafood.service.AtivacaoClienteService;
+
 /**
  * @Controller - torna esta classe um bean que o spring ira gerenciar
  */
 @Controller
 public class MeuController {
 	
-	public MeuController() {
-		System.out.println("MeuController");
-	}
+	private AtivacaoClienteService ativacaoCliente;
 	
+	public MeuController(AtivacaoClienteService ativacaoCliente) {
+		this.ativacaoCliente = ativacaoCliente;
+		System.out.println("MeuController");
+		System.out.println(ativacaoCliente );
+	}
+
 	/**
 	 * @GetMapping("/hello") - mapeando a forma de acesso a esse endpoint, GET/hello
 	 * @ResponseBody - informa que o resultado sera retornado como resposta da requisicao 
@@ -21,6 +28,11 @@ public class MeuController {
 	@GetMapping("/hello")
 	@ResponseBody
 	public String hello() {
+		
+		Cliente jhonatan = new Cliente("jhonatan@gmail.com", "Jhonatan", "61999999999");
+		
+		ativacaoCliente.ativar(jhonatan);
+		
 		return "Funcionando teste devtolls................";
 	}
 
---
+++ algaworks-especialista-rest/algafood-api/src/main/java/br/com/jkavdev/algaworks/algafood/service/AtivacaoClienteService.java
@@ -0,0 +1,24 @@
+package br.com.jkavdev.algaworks.algafood.service;
+
+import org.springframework.stereotype.Component;
+
+import br.com.jkavdev.algaworks.algafood.modelo.Cliente;
+import br.com.jkavdev.algaworks.algafood.notificacao.Notificador;
+
+@Component
+public class AtivacaoClienteService {
+
+	private Notificador notificador;
+
+	public AtivacaoClienteService(Notificador notificador) {
+		super();
+		this.notificador = notificador;
+		System.out.println(notificador);
+	}
+
+	public void ativar(Cliente cliente) {
+		cliente.ativar();
+		notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
+	}
+
+}
