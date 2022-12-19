dcb805bf2061b62f88299244dda5564e7f7f7eb3
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2022-12-19 01:37:47 -0300

    Algaworks - Especialista Spring REST - 2.17. Desambiguação de beans com @Primary

--- algaworks-especialista-rest/algafood-api/src/main/java/br/com/jkavdev/algaworks/algafood/notificacao/NotificadorEmail.java
+++ algaworks-especialista-rest/algafood-api/src/main/java/br/com/jkavdev/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -1,9 +1,17 @@
 package br.com.jkavdev.algaworks.algafood.notificacao;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import br.com.jkavdev.algaworks.algafood.modelo.Cliente;
 
+/**
+ * @Primary - define que este bean tera prioridade na hora de criar um bean de Notificador
+ * ignorando os outros possiveis beans
+ *
+ */
+
+@Primary
 @Component
 public class NotificadorEmail implements Notificador {
 
--- algaworks-especialista-rest/algafood-api/src/main/java/br/com/jkavdev/algaworks/algafood/service/AtivacaoClienteService.java
+++ algaworks-especialista-rest/algafood-api/src/main/java/br/com/jkavdev/algaworks/algafood/service/AtivacaoClienteService.java
@@ -1,7 +1,5 @@
 package br.com.jkavdev.algaworks.algafood.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,18 +10,19 @@ import br.com.jkavdev.algaworks.algafood.notificacao.Notificador;
 public class AtivacaoClienteService {
 
 	/**
-	 * como temos varias implementacoes de Notificador, o spring nao sabe qual injetar
-	 * uma das formas de contornar o erro, eh transformando em uma lista de beans
+	 * como temos varias implementacoes de Notificador, o spring nao sabe qual
+	 * injetar uma das formas de contornar o erro, eh transformando em uma lista de beans
 	 */
+//	@Autowired
+//	private List<Notificador> notificadores;
+	
 	@Autowired
-	private List<Notificador> notificadores;
+	private Notificador notificador;
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-		for (Notificador notificador : notificadores) {
-			notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
-		}
+		notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
 
 	}
 
