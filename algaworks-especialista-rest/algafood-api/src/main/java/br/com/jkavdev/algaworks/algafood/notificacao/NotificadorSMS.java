5963e5877e7555fd5662aac0036d001422c4fc90
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2022-12-19 01:35:04 -0300

    Algaworks - Especialista Spring REST - 2.16. Ambiguidade de beans e injeção de lista de beans

---
+++ algaworks-especialista-rest/algafood-api/src/main/java/br/com/jkavdev/algaworks/algafood/notificacao/NotificadorSMS.java
@@ -0,0 +1,15 @@
+package br.com.jkavdev.algaworks.algafood.notificacao;
+
+import org.springframework.stereotype.Component;
+
+import br.com.jkavdev.algaworks.algafood.modelo.Cliente;
+
+@Component
+public class NotificadorSMS implements Notificador {
+
+	@Override
+	public void notificar(Cliente cliente, String mensagem) {
+		System.out.printf("Notificando %s através do SMS %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
+	}
+
+}
--- algaworks-especialista-rest/algafood-api/src/main/java/br/com/jkavdev/algaworks/algafood/service/AtivacaoClienteService.java
+++ algaworks-especialista-rest/algafood-api/src/main/java/br/com/jkavdev/algaworks/algafood/service/AtivacaoClienteService.java
@@ -1,5 +1,7 @@
 package br.com.jkavdev.algaworks.algafood.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,15 +11,17 @@ import br.com.jkavdev.algaworks.algafood.notificacao.Notificador;
 @Component
 public class AtivacaoClienteService {
 
+	/**
+	 * como temos varias implementacoes de Notificador, o spring nao sabe qual injetar
+	 * uma das formas de contornar o erro, eh transformando em uma lista de beans
+	 */
 	@Autowired
-	private Notificador notificador;
+	private List<Notificador> notificadores;
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-		if (notificador == null) {
-			System.out.println("Nao existe notificar mas cliente foi ativado");
-		} else {
+		for (Notificador notificador : notificadores) {
 			notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
 		}
 
