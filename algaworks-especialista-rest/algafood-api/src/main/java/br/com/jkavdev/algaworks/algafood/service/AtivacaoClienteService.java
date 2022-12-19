39013807f2d29fcc71df533f4816b6646776488e
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2022-12-19 09:15:09 -0300

    Algaworks - Especialista Spring REST - 2.21. Criando métodos de callback do ciclo de vida do beans

--- algaworks-especialista-rest/algafood-api/src/main/java/br/com/jkavdev/algaworks/algafood/service/AtivacaoClienteService.java
+++ algaworks-especialista-rest/algafood-api/src/main/java/br/com/jkavdev/algaworks/algafood/service/AtivacaoClienteService.java
@@ -1,16 +1,23 @@
 package br.com.jkavdev.algaworks.algafood.service;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import br.com.jkavdev.algaworks.algafood.modelo.Cliente;
 import br.com.jkavdev.algaworks.algafood.notificacao.NivelUrgerncia;
 import br.com.jkavdev.algaworks.algafood.notificacao.Notificador;
 import br.com.jkavdev.algaworks.algafood.notificacao.TipoDoNotificador;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
+/**
+ * implements DisposableBean, InitializingBean - implementando interfaces que disponibilizam callbacks
+ *
+ */
 @Component
-public class AtivacaoClienteService {
+public class AtivacaoClienteService implements DisposableBean, InitializingBean {
 
 	/**
 	 * como temos varias implementacoes de Notificador, o spring nao sabe qual
@@ -30,12 +37,36 @@ public class AtivacaoClienteService {
 	@TipoDoNotificador(NivelUrgerncia.NORMAL)	
 	@Autowired
 	private Notificador notificador;
+	
+	/**
+	 * @PostConstruct - sera chamado depois da construcao do bean 
+	 */
+	@PostConstruct
+	public void init() {
+		System.out.println("init");
+	}
+	
+	/**
+	 * @PostConstruct - sera chamado antes da destruicao do bean 
+	 */
+	@PreDestroy
+	public void destroy() {
+		System.out.println("destroy");
+	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
 
+	}
+
+	/**
+	 * afterPropertiesSet - sera chamado depois da construcao do bean
+	 */
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("afterPropertiesSet");
 	}
 
 }
